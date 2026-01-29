098be546732ea46ae99c36ee6c522a678907f5f1
kazukingrad-jpg <kazuking.rad@gmail.com>
2026-01-29 18:13:38 +0900

    commit3

---
+++ db/migrate/20260129083307_create_items.rb
@@ -0,0 +1,20 @@
+class CreateItems < ActiveRecord::Migration[7.1]
+  def change
+    create_table :items do |t|
+
+      t.string :title, null: false
+      t.text :content, null: false
+
+      t.references :user, null: false
+
+      t.integer :category_id,             null: false
+      t.integer :condition_id,            null: false
+      t.integer :shipping_fee_burden_id,  null: false
+      t.integer :prefecture_id,           null: false
+      t.integer :shipping_day_id,         null: false
+      t.integer :price,                   null: false
+
+      t.timestamps
+    end
+  end
+end
