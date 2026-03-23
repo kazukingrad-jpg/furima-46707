f30eb97cfebee825179ca7a205b81da5f1b91ed0
kazukingrad-jpg <kazuking.rad@gmail.com>
2026-03-23 15:29:26 +0900

    修正2

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,15 +1,3 @@
 class Address < ApplicationRecord
   belongs_to :order
-
-  with_options presence: true do
-    validates :post_code
-    validates :prefecture_id
-    validates :city
-    validates :street_address
-    validates :phone_number
-  end
-
-  validates :post_code, format: { with: /\A\d{3}-\d{4}\z/ }
-  validates :prefecture_id, numericality: { other_than: 1 }
-  validates :phone_number, format: { with: /\A\d{10,11}\z/ }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :items, dependent: :destroy
+  # Userモデル差分追記
   has_many :orders, dependent: :destroy
  
  
